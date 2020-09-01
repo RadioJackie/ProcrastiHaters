60caa089242a8c8f8477179fcdb7d44d041f7ab1
dpayton92 <dpayton92@gmail.com>
2020-09-01 09:32:35 -0700

    undated button

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-
-import React from "react";
-=======
 // import React from "react";
 // import { Link } from "react-router-dom";
 // import "./style.css";
->>>>>>> d1d31b98c37f01c0bbd7b6634da798a4ee817279
 
 // function Button() {
 //     return (
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-// import { Button } from 'react-bootstrap';
-=======
 //import { Button } from 'react-strap';
->>>>>>> d1d31b98c37f01c0bbd7b6634da798a4ee817279
 function Nav() {
   return (
     <div>
