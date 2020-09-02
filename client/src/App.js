d2f123d90d77da25db21bb0739981f1ea55e82f8
dpayton92 <dpayton92@gmail.com>
2020-09-01 21:29:49 -0700

    updated Carousel Pic and add Calendar JS

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,14 @@ import Carousel from "./components/carousel";
 import NavBar from "./components/NavBar"
 // import Navbar from "./components/NavBar";
 
+
 function App() {
   return (
     <Router>
       <div>
       <Carousel />
       <NavBar />
-        {/* <Nav /> */}
+        <Nav />
         <Switch>
           {/* <Route exact path={["/", "/quotes"]}>
             <Quotes />
