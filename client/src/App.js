f4abd7da8e562151b061540cb4530d524e73228d
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-09-12 12:06:37 -0500

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Quote from "./pages/Quote";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
+// import Detail from "./pages/Detail";
+// import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import Carousel from "./components/carousel";
 import NavBar from "./components/NavBar"
-import MyCalendar from "./components/Calendar"
+// import MyCalendar from "./components/Calendar"
 // import Navbar from "./components/NavBar";
-import Qdata from "./Qdata"
-import Error from './Error'
+// import Qdata from "./Qdata"
+// import Error from './Error'
 import './App.css'
 
 
