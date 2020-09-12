036a257bff286410531f4122f5d01e2274fc6bfa
dpayton92 <dpayton92@gmail.com>
2020-09-12 06:56:33 -0700

    signup and login page

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+ 
+import Signup from 'react-mua-signup';
+import 'react-mua-signup/dist/index.css'
+ 
+ReactDOM.render(<Signup onSignup={(email, p) => {
+  console.debug(email)
+  console.debug(p)
+}}/>, document.getElementById('root'))
+
+export default Signup
